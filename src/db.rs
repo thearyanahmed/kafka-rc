454d23014821c0c0d97f4f0ef062bfad298b73bc
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-04-25 05:16:48 +0600

    Added config module

---
+++ src/db.rs
@@ -0,0 +1,108 @@
+use secrecy::{Secret, ExposeSecret};
+use serde_aux::field_attributes::deserialize_number_from_string;
+use std::convert::{TryInto, TryFrom};
+use sqlx::postgres::{PgConnectOptions, PgSslMode};
+use sqlx::ConnectOptions;
+
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub application: ApplicationSettings,
+    pub database: DatabaseSettings,
+}
+
+pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub port: u16,
+    pub host: String,
+    pub base_url: String,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub driver: String,
+    pub username: String,
+    pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+    pub require_ssl: bool,
+}
+
+pub enum Environment {
+    Local,
+    Production
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "local"
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is nota supported environment. use either 'local' or 'production'", other
+            ))
+        }
+    }
+}
+
+pub fn get_configuration() -> Result<Settings,config::ConfigError> {
+    let mut settings = config::Config::default();
+
+    let base_path = std::env::current_dir().expect("failed to determine the current directory.");
+    let config_dir = base_path.join("configuration");
+
+    settings.merge(config::File::from(
+        config_dir.join("base")
+        ).required(true)
+    )?;
+
+    let env : Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("failed to parse APP_ENVIRONMENT");
+
+    let _ = settings.merge(
+        config::File::from(config_dir.join(env.as_str())).required(true)
+    );
+
+    let _ = settings.merge(config::Environment::with_prefix("app").separator("__"));
+
+    settings.try_into()
+}
+
+impl DatabaseSettings {
+    pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            PgSslMode::Prefer
+        };
+
+        PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.username)
+            .password(&self.password.expose_secret())
+            .port(self.port)
+            .ssl_mode(ssl_mode)
+    }
+
+    pub fn with_db(&self) -> PgConnectOptions {
+        let mut options = self.without_db().database(&self.database_name);
+
+        options.log_statements(tracing::log::LevelFilter::Trace);
+
+        options
+    }
+}
