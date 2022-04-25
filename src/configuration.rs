271a07600d84fa580d929c647721e7b0c4bc70ae
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-04-25 06:00:21 +0600

    Added .env support

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,12 +1,15 @@
 #[derive(serde::Deserialize, Debug)]
 pub struct Config {
     host: String,
+    hello: Hello,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct Hello {
     port: u16
 }
 
 pub fn get_configuration() -> Result<Config,envy::Error> {
-    dotenv::dotenv().expect("Failed to read .env file");
-
     return match envy::from_env::<Config>() {
         Ok(config) => Ok(config),
         Err(e) => Err(e),
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use clockwork::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().expect("Failed to read .env file");
+    
     let config = get_configuration().expect("could not read .env file.");
 
     println!("config {:?}",config);
