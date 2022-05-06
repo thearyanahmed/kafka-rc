39f0e4454fe143994a6cd85d1e242ff15dc57264
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-05-07 00:44:34 +0600

    Added scope based routing

--- src/health_check/mod.rs
+++ src/health_check/mod.rs
@@ -1,9 +1,18 @@
-use actix_web::{Scope, web};
+use actix_web::{guard, HttpResponse, Resource, Scope, web};
+use actix_web::dev::WebService;
 use crate::health_check::routes::health_check;
 
 mod routes;
 
 pub fn register() -> Scope {
 	web::scope("/api/v1")
-		.route("health-check",web::get().to(health_check))
+		.service(
+			web::resource("/health-check").route(
+				web::route()
+					.guard(guard::Get())
+					// .guard(guard::Header("content-type", "application/json"))
+					.to(health_check),
+			)
+		)
+
 }
